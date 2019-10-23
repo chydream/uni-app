33aef8af972567acc205e5e6a6d15db94b408c3e
chenyong <yong.chen@doone.com.cn>
2019-10-23 08:16:59 +0800

    更新

--- App.vue
+++ App.vue
@@ -23,4 +23,8 @@
 	// 		display: none;
 	// 	}
 	// }
+	html,body{
+		width: 100%;
+		height: 100%;
+	}
 </style>
--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App'
 import './mixins/install'
 import store from './store'
+import './util/rem'
 Vue.config.productionTip = false
 Vue.prototype.$store = store
 App.mpType = 'app'
